18782620891ddcd812e066158dc79f83fe50b1ec
Katja Röhlig <katja-roehlig@mail.de>
2026-01-22 17:01:42 +0100

    refactor main function

--- filter_users.py
+++ filter_users.py
@@ -2,6 +2,11 @@ import json
 
 
 def filter_users_by_name(name):
+    """
+    filters the user list by a given name and prints the user data
+
+    :param name: user name
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -13,6 +18,11 @@ def filter_users_by_name(name):
 
 
 def filter_users_by_age(age):
+    """
+    filters the user list by a given age and prints the user data
+
+    :param age:
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -25,6 +35,11 @@ def filter_users_by_age(age):
 
 
 def filter_users_by_email(email):
+    """
+    filters the user list by a given email and prints the user data
+
+    :param email: Beschreibung
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -35,7 +50,7 @@ def filter_users_by_email(email):
         print(user)
 
 
-if __name__ == "__main__":
+def main():
     filter_option = (
         input(
             "What would you like to filter by? (Currently 'name', 'age' and 'email' are supported): "
@@ -48,24 +63,26 @@ if __name__ == "__main__":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
 
-    if filter_option == "age":
+    elif filter_option == "age":
         while True:
             try:
                 age_to_search = int(input("Enter an age to filter users: ").strip())
                 filter_users_by_age(age_to_search)
-                break
+                return
             except ValueError:
                 print("Please enter a number:")
 
-    if filter_option == "email":
+    elif filter_option == "email":
         while True:
             email_to_search = input("Enter an email to filter users: ").strip()
             if "@" in email_to_search and email_to_search[-4:] == ".com":
                 filter_users_by_email(email_to_search)
-                break
+                return
             else:
                 print("Please enter a valid email: ")
+    else:
+        print("Filtering by that option is not yet supported.")
 
 
-else:
-    print("Filtering by that option is not yet supported.")
+if __name__ == "__main__":
+    main()
